70c0d8cd1d5b254a9af89a928256dfaf056b8aa0
Ioannis Filippidis <jfilippidis@gmail.com>
2014-05-04 03:16:44 -0700

    use parentheses for multiple imports

--- polytope/__init__.py
+++ polytope/__init__.py
@@ -35,10 +35,12 @@
 # KEEP THIS NUMBER IN SYNC WITH THAT IN setup.py
 __version__ = '0.1.0'
 
-from .polytope import Polytope, Region
-from .polytope import is_empty, is_fulldim, is_convex, is_adjacent, is_subset
-from .polytope import reduce, separate, box2poly, grid_region
-from .polytope import cheby_ball, bounding_box, envelope, extreme, qhull
-from .polytope import is_inside, union, mldivide, intersect, volume, projection
+from .polytope import (
+    Polytope, Region,
+    is_empty, is_fulldim, is_convex, is_adjacent, is_subset,
+    reduce, separate, box2poly, grid_region,
+    cheby_ball, bounding_box, envelope, extreme, qhull,
+    is_inside, union, mldivide, intersect, volume, projection
+)
 from .plot import plot_partition, plot_transition_arrow
 from .prop2partition import Partition, MetricPartition, find_adjacent_regions
