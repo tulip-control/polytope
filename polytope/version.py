1b5012c6ed165509039655f57338f2fd46eea7cb
Scott Livingston <slivingston@cds.caltech.edu>
2014-07-05 10:36:54 -0700

    Version suffix of -dev-{%H,unknown-commit} for non-release installations
    We now use the same method for version labeling here as in the TuLiP
    repository, sans some backwards-compatibility stuff required in TuLiP.
    
    The reference commit in the TuLiP repository is
    71aefd0659904ccdb8553e5d5f2436933ea42728

--- polytope/version.py
+++ polytope/version.py
@@ -35,3 +35,30 @@ polytope package version
 version_info = (0, 1, 1)
 
 version = '.'.join([str(x) for x in version_info])
+
+
+# Append annotation to version string to indicate development versions.
+#
+# An empty (modulo comments and blank lines) commit_hash.txt is used
+# to indicate a release, in which case nothing is appended to version
+# string as defined above.
+import os.path
+path_to_hashfile = os.path.join(os.path.dirname(__file__), "commit_hash.txt")
+if os.path.exists(path_to_hashfile):
+    commit_hash = ""
+    with open(path_to_hashfile, "r") as f:
+        for line in f:
+            line = line.strip()
+            if len(line) == 0 or line[0] == '#':
+                # Ignore blank lines and comments, the latter being
+                # any line that begins with #.
+                continue
+
+            # First non-blank line is assumed to be the commit hash
+            commit_hash = line
+            break
+
+    if len(commit_hash) > 0:
+        version += "-dev-" + commit_hash
+else:
+    version += "-dev-unknown-commit"
