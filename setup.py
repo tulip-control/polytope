83dfbd35789d0cac92433e3f9b7d14f46244da37
Ioannis Filippidis <jfilippidis@gmail.com>
2016-08-26 02:26:27 -0700

    REL: add Python 3 and 3.5 to Trove classifiers to signify support

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ classifiers = [
     'Programming Language :: Python',
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
     'Topic :: Scientific/Engineering',
     'Topic :: Software Development']
 
