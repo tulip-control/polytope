fcf751a04ca0b10761163b222c6e62afbd1dcbdc
Scott C. Livingston <slivingston@cds.caltech.edu>
2017-04-17 00:04:18 -0400

    BUGFIX: Treat case of multiple polytopes to subtract
    If more than two polytopes in a region (defined by a collection of
    polytopes) is active in the subtraction performed by region_diff(),
    then the second element in the argument to a `vstack` call might
    have more than one element, resulting in dimension mismatch.
    Thus, use `hstack` instead.

--- polytope/polytope.py
+++ polytope/polytope.py
@@ -2132,7 +2132,7 @@ def region_diff(poly, reg, abs_tol=ABS_TOL, intersect_tol=ABS_TOL,
     M = np.sum(mi)
 
     if len(mi[0:len(mi)-1]) > 0:
-        csum = np.cumsum(np.vstack([0, mi[0:len(mi)-1]]))
+        csum = np.cumsum(np.hstack([0, mi[0:len(mi)-1]]))
         beg_mi = csum + m*np.ones(len(csum), dtype=int)
     else:
         beg_mi = np.array([m])
