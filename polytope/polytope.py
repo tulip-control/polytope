379afa3304a4b24a0cb24089e5e092365422f4b8
Ioannis Filippidis <jfilippidis@gmail.com>
2017-10-06 09:28:25 -0700

    MAI: add missing import

--- polytope/polytope.py
+++ polytope/polytope.py
@@ -71,6 +71,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import logging
+import warnings
 
 import numpy as np
 from scipy import optimize
