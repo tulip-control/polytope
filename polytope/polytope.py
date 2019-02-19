b478461891c2bfc4eebbfc098628f57ee0752dea
samuelkolb <samuelkolb@users.noreply.github.com>
2019-02-19 11:35:59 +0100

    Customizable plotting + removed deprecated logger.warn calls (#53)
    * Made plotting customizable
    * Take as default values the forced values from before this pull request
    * Remove deprecated calls to logger.warn; replaced calls with  logger.warning

--- polytope/polytope.py
+++ polytope/polytope.py
@@ -400,10 +400,15 @@ class Polytope(object):
             self.bbox = bounding_box(self)
         return self.bbox
 
-    def plot(self, ax=None, color=None,
-             hatch=None, alpha=1.0):
+    def plot(self, ax=None, color=None, hatch=None, alpha=1.0, linestyle=None, linewidth=None, edgecolor=None):
         if self.dim != 2:
             raise Exception("Cannot plot polytopes of dimension larger than 2")
+        
+        # Setting default values for plotting
+        linestyle = linestyle or "dashed"
+        linewidth = linewidth or 3
+        edgecolor = edgecolor or "black"
+        
         ax = _newax(ax)
         if not is_fulldim(self):
             logger.error("Cannot plot empty polytope")
@@ -412,8 +417,8 @@ class Polytope(object):
             color = np.random.rand(3)
         poly = _get_patch(
             self, facecolor=color, hatch=hatch,
-            alpha=alpha, linestyle='dashed', linewidth=3,
-            edgecolor='black')
+            alpha=alpha, linestyle=linestyle, linewidth=linewidth,
+            edgecolor=edgecolor)
         ax.add_patch(poly)
         return ax
 
@@ -856,8 +861,7 @@ class Region(object):
             self.bbox = bounding_box(self)
         return self.bbox
 
-    def plot(self, ax=None, color=None,
-             hatch=None, alpha=1.0):
+    def plot(self, ax=None, color=None, hatch=None, alpha=1.0, linestyle=None, linewidth=None, edgecolor=None):
         """Plot a `polytope` on axes `ax`."""
         # TODO optional arg for text label
         if self.dim != 2:
@@ -870,7 +874,8 @@ class Region(object):
             color = np.random.rand(3)
         for poly2 in self.list_poly:
             # TODO hatched polytopes in same region
-            poly2.plot(ax, color=color, hatch=hatch, alpha=alpha)
+            poly2.plot(ax, color=color, hatch=hatch, alpha=alpha, linestyle=linestyle, linewidth=linewidth,
+                       edgecolor=edgecolor)
         return ax
 
     def text(self, txt, ax=None, color='black'):
