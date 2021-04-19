f6bad2a0154b1588abab437490df84e4cabf4d61
Jonas Krook <jonas.krook@zenseact.com>
2021-04-19 13:28:13 +0200

    REF: improve readability of function `region_diff`

--- polytope/polytope.py
+++ polytope/polytope.py
@@ -2056,8 +2056,8 @@ def region_diff(poly, reg, abs_tol=ABS_TOL, intersect_tol=ABS_TOL,
         return Polytope()
     # some constraints are active
     M = np.sum(mi)
-    if len(mi[0:len(mi) - 1]) > 0:
-        csum = np.cumsum(np.hstack([0, mi[0:len(mi) - 1]]))
+    if len(mi) > 1:
+        csum = np.cumsum(np.hstack([0, mi[0:-1]]))
         beg_mi = csum + m * np.ones(len(csum), dtype=int)
     else:
         beg_mi = np.array([m])
@@ -2093,12 +2093,12 @@ def region_diff(poly, reg, abs_tol=ABS_TOL, intersect_tol=ABS_TOL,
                     INDICES = np.hstack([INDICES, beg_mi[level] + M])
                     break
             if R < abs_tol:
-                level = level - 1
+                level -= 1
                 res = union(res, Polytope(A[INDICES, :], B[INDICES]), False)
                 nzcount = np.nonzero(counter)[0]
                 for jj in xrange(len(nzcount) - 1, -1, -1):
                     if counter[level] <= mi[level]:
-                        INDICES[-1] = INDICES[-1] - M
+                        INDICES[-1] -= M
                         INDICES = np.hstack([
                             INDICES,
                             beg_mi[level] + counter[level] + M
@@ -2117,9 +2117,9 @@ def region_diff(poly, reg, abs_tol=ABS_TOL, intersect_tol=ABS_TOL,
             nzcount = np.nonzero(counter)[0]
             for jj in xrange(len(nzcount) - 1, -1, -1):
                 level = nzcount[jj]
-                counter[level] = counter[level] + 1
+                counter[level] += 1
                 if counter[level] <= mi[level]:
-                    INDICES[-1] = INDICES[-1] - M
+                    INDICES[-1] -= M
                     INDICES = np.hstack([
                         INDICES,
                         beg_mi[level] + counter[level] + M - 1
@@ -2128,7 +2128,7 @@ def region_diff(poly, reg, abs_tol=ABS_TOL, intersect_tol=ABS_TOL,
                 else:
                     counter[level] = 0
                     INDICES = INDICES[0:m + np.sum(counter)]
-                    level = level - 1
+                    level -= 1
                     if level == -1:
                         if save:
                             if save:
@@ -2146,7 +2146,7 @@ def region_diff(poly, reg, abs_tol=ABS_TOL, intersect_tol=ABS_TOL,
             if level == N - 1:
                 res = union(res, reduce(test_poly), False)
             else:
-                level = level + 1
+                level += 1
     logger.debug('returning res from end')
     return res
 
