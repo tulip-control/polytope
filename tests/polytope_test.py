3de5a80929acd59322ca806268328b5c55a3806d
Scott C. Livingston <slivingston@cds.caltech.edu>
2024-09-30 16:33:58 -0700

    TEST: prefer assert_allclose; correct arg order
    Use assert_allclose() instead of assert_almost_equal(), following
    recommendation in documentation of assert_almost_equal [1].
    Swap order of the arguments such that Gurobi output is first;
    it is the "actual" value that we compare to the "desired" SciPy [2].
    
    [1] https://numpy.org/doc/stable/reference/generated/numpy.testing.assert_almost_equal.html
    [2] https://numpy.org/doc/stable/reference/generated/numpy.testing.assert_equal.html

--- tests/polytope_test.py
+++ tests/polytope_test.py
@@ -3,8 +3,8 @@
 import logging
 
 import numpy as np
-from numpy.testing import assert_allclose
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_allclose, assert_array_equal, assert_equal
+
 import pytest
 import scipy.optimize
 
@@ -620,9 +620,9 @@ def test_gurobipy_return_same_result_as_scipy():
     c, A, b = example_1d()
     result_gurobi = solvers.lpsolve(c, A, b, solver='gurobi')
     result_scipy = solvers.lpsolve(c, A, b, solver='scipy')
-    nt.assert_equal(result_scipy['status'], result_gurobi['status'])
-    nt.assert_almost_equal(result_scipy['x'][0], result_gurobi['x'][0])
-    nt.assert_almost_equal(result_scipy['fun'], result_gurobi['fun'])
+    assert_equal(result_gurobi['status'], result_scipy['status'])
+    assert_allclose(result_gurobi['x'][0], result_scipy['x'][0])
+    assert_allclose(result_gurobi['fun'], result_scipy['fun'])
 
 
 if __name__ == '__main__':
